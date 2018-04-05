a4d1898c3bde6ca44a058de5bc2b06d485d9ac05
tatref <tatref@github.com>
2018-04-06 00:26:58 +0200

    fix docker group

--- host/setup.sh
+++ host/setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32f99834a24d1c956c980a046765b41f57e00726b03d67294082e1b47002a05b
-size 1287
+oid sha256:4ffd88693553e3f500f7411eff9a4f1b6aac40d7e5f7a85b80e04b0b6f8f5d3e
+size 1405
