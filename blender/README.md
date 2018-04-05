330a9110c9c2b896649383302ea8e887a441a9d9
tatref <tatref@github.com>
2018-04-05 22:54:19 +0200

    add vnc setup

---
+++ blender/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6fe3390b95a261d4949b43890b11bad6a61a3fca29acbf52a08188dd5d73c0ef
+size 645
--- host/setup.sh
+++ host/setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1beaf0fea51c536bb0211414be98722e44698ff6fc632b26533d5baa7cb7bbc2
-size 871
+oid sha256:32f99834a24d1c956c980a046765b41f57e00726b03d67294082e1b47002a05b
+size 1287
