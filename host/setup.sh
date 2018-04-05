d5e6def8e6f329ff0aae13bc3f78c98940f5c3ac
tatref <tatref@github.com>
2018-04-06 00:43:05 +0200

    fix sudo

--- host/setup.sh
+++ host/setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dec17e247cff901a898ad79746542b833b95e680185c93682bb5cd7ccea7b489
-size 1525
+oid sha256:76eb03a2cb5682fd9d767f6ec8a73f12e1546c282272d7a1c4f037bc5923178e
+size 1479
