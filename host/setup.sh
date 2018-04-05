0b42a9941fc7fb0937471b01b7dee4627c5ea889
tatref <tatref@github.com>
2018-04-06 00:34:26 +0200

    fix turbovnc install

--- host/setup.sh
+++ host/setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44beb952c870798474726202c02c83890f0f18ab63e7db1956838e620212de0d
-size 1469
+oid sha256:ac9983716272e4bcd2394c61ad79619c4caede878afc961ac6b24c05e232a332
+size 1489
