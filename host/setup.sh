e06025cdde991a094194efaabeb3730ae0f8a358
tatref <tatref@github.com>
2018-04-06 00:37:14 +0200

    proper fix

--- host/setup.sh
+++ host/setup.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de7ef1548391001e0bd40a8f7873d8962e3507bf980e540d4236695843742b74
+oid sha256:89c0653b0af5803df58ac1812cfe5cc3cda8a54efe6ace8e0149d9649fb5ac0a
 size 1504
