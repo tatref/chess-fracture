b0c0c18adaaa4a62c97b167b331aba18aaa2bc93
tatref <tatref@users.noreply.github.com>
2018-04-09 13:58:07 +0200

    add materials

--- blender/chess_fracture.py
+++ blender/chess_fracture.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3787ac8ad44ebdcbbfa3ae3b03d83cf617f2cd9cde1419f8bf925ff236b19e7
-size 14487
+oid sha256:e153230df8fb3dad05570731f5b4c5ba0d67203e1ace6c808ae409a14facb55a
+size 14915
