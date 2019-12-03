a4bca92c2bb33ad5b733a490530428a01ca5930f
tatref <tatref@github.com>
2019-12-03 01:29:19 +0100

    blender 2.81

--- django/worker.py
+++ django/worker.py
@@ -68,7 +68,7 @@ def compress_file(f_in):
     return mem
 
 def run_simulation(pgn_path, out_blend, display=':1'):
-    blender_exe = '/home/{}/blender-2.80-linux-glibc217-x86_64/blender'.format(os.environ['USER'])
+    blender_exe = '/home/{}/blender-2.81-linux-glibc217-x86_64/blender'.format(os.environ['USER'])
     blend_template = '/home/{}/chessfracture/blender/chess_fracture_template_2.80.blend'.format(os.environ['USER'])
     blender_script = '/home/{}/chessfracture/blender/chess_fracture_2.80.py'.format(os.environ['USER'])
     timeout = 200
